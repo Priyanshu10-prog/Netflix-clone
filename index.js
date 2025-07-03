aceace0849a5e84d9d587f2c98dc9af12e473c51
Your Name <you@example.com>
2025-07-03 13:28:35 +0530

    basic netflix frontend

---
+++ index.js
@@ -0,0 +1,6 @@
+document.querySelectorAll(".dropdown-item")[0].addEventListener("click",function(){
+    document.querySelector(".language").textContent="Hindi";
+})
+document.querySelectorAll(".dropdown-item")[1].addEventListener("click",function(){
+    document.querySelector(".language").textContent="English";
+})
